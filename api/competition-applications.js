70df1cbf99267cab2b15812722304ab922e9529a
GlenMue <85568877+GlenMue@users.noreply.github.com>
2026-06-01 20:21:55 +0100

    Test validation strings

--- api/competition-applications.js
+++ api/competition-applications.js
@@ -1,4 +1,6 @@
+const message = "email is required";
+
 export default function handler(req, res) {
   res.statusCode = 200;
-  res.end(JSON.stringify({ ok: true }));
+  res.end(JSON.stringify({ ok: true, message }));
 }
