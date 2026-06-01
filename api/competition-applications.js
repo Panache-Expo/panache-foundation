bcd88d805c857ce736174bc8296366751f1d011f
GlenMue <85568877+GlenMue@users.noreply.github.com>
2026-06-01 20:29:13 +0100

    Test env only

--- api/competition-applications.js
+++ api/competition-applications.js
@@ -1,6 +1,9 @@
 import { createClient } from "\u0040supabase/supabase-js";
 
+const SUPABASE_URL = "";
+const SUPABASE_KEY = "";
+
 export default function handler(req, res) {
   res.statusCode = 200;
-  res.end(JSON.stringify({ ok: true, hasClient: Boolean(createClient) }));
+  res.end(JSON.stringify({ ok: Boolean(createClient), hasUrl: Boolean(SUPABASE_URL), hasKey: Boolean(SUPABASE_KEY) }));
 }
