c78a8c660c4bc849c33a338f99af318a81e7bf76
GlenMue <85568877+GlenMue@users.noreply.github.com>
2026-06-01 20:25:43 +0100

    Test import restore

--- api/competition-applications.js
+++ api/competition-applications.js
@@ -1,6 +1,6 @@
-const message = "email is required";
+import { createClient } from "\u0040supabase/supabase-js";
 
 export default function handler(req, res) {
   res.statusCode = 200;
-  res.end(JSON.stringify({ ok: true, message }));
+  res.end(JSON.stringify({ ok: true, hasClient: Boolean(createClient) }));
 }
